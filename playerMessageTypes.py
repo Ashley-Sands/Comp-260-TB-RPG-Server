58fae85d39a2221d8c990690dd57d296cb90b2f1
Ashley Sands <ashsands1@amstrike.com>
2020-03-12 17:54:54 +0000

    added player id to move player

--- playerMessageTypes.py
+++ playerMessageTypes.py
@@ -4,6 +4,15 @@ from constants import *
 class PlayerMessageTypes:
 
     @staticmethod
-    def move_player(from_id, x, y, z):
+    def move_player(from_id, player_id, x, y, z):
+        """
+
+        :param from_id:
+        :param player_id:
+        :param x:
+        :param y:
+        :param z:
+        :return:
+        """
 
         return locals()
