e415931532b4cfd0165e4ff9ed4bb67c0509e56e
Ashley Sands <ashsands1@amstrike.com>
2020-03-20 14:17:10 +0000

    added base socket client

--- Sockets/BaseSocket.py
+++ Sockets/BaseSocket.py
@@ -1,1 +1,81 @@
-import
+import Common.DEBUG as DEBUG
+import Common.constants as constants
+import threading
+
+class BaseSocketClient:
+    """Socket clients are used handle the clients inbound/outbound messages."""
+
+    MESSAGE_LEN_PACKET_SIZE = 2  # bytes
+    MESSAGE_TYPE_PACKET_SIZE = 1  # byte
+    BYTE_ORDER = "big"
+
+    def __init__( self, socket ):
+
+        self.socket = socket
+        self.db_client_key = -1
+
+        self.thread_lock = threading.Lock()
+        self.inbound_thread = None
+        self.outbound_thread = None
+
+        self._started = False
+        self._valid = True
+
+    def start( self ):
+        """
+        Called to allow the inbound and outbound thread to be fired up
+        Override to define how the threads should be started
+        """
+
+        self._started = True
+
+    def valid( self, is_valid=None ):
+        """
+        Thread safe method tp get and set _valid
+        If is_valid is None, it is not set
+        """
+
+        self.thread_lock.acquire()
+
+        if is_valid is not None:
+            self._valid = is_valid
+
+        is_valid = self._valid
+
+        self.thread_lock.release()
+
+        return is_valid
+
+    def from_name ():
+        """ What should be displayed as from name when sending message from this socket.
+            By default this is 'SERVER' (constants.SERVER_NAME)
+        """
+        return constants.SERVER_NAME
+
+    ## ...
+
+    def close_socket( self ):
+
+        self.socket.close()
+
+
+    def join_threads( self ):
+        """
+            joins both inbound and outbound threads back onto the main if needed.
+            Be sure to of called closed_socket, to allow the sockets to exit.
+        """
+        if self.inbound_thread.is_alive():
+            self.inbound_thread.join()
+
+        if self.outbound_thread is not None and self.outbound_thread.is_alive():
+            self.outbound_thread.join()
+
+    def close( self ):
+        """
+            Closes the sockets and joins the threads
+            Override close_socket and join_threads to extend
+        """
+        self.valid( False )
+
+        self.close_socket()
+        self.join_threads()
