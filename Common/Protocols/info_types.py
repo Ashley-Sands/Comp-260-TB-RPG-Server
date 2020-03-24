ab5c81e359627f6e0f7a0ee5d0877899fa4ee527
Ashley Sands <ashsands1@amstrike.com>
2020-03-24 21:17:26 +0000

    added new lobby protocol

---
+++ Common/Protocols/info_types.py
@@ -0,0 +1,7 @@
+# the functions used to enforce the message types
+# info types are things like lobby and player list ect...
+# every think must contain a from client name
+
+# See Protocol list for details
+def lobby_list( from_client_name, server_names, game_ids, current_clients, max_clients ):
+    return locals()
