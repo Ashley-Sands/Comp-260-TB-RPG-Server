7eafbf1237a704824632c88de5f408ef9fd67b27
Ashley Sands <ashsands1@amstrike.com>
2020-03-27 15:10:03 +0000

    added ping protocol

---
+++ Common/Protocols/test.py
@@ -0,0 +1,7 @@
+# the functions used to enforce the message types
+# every think must contain a from client name
+
+# See Protocol list for details
+
+def ping( from_client_id, client_send_time, server_receive_time ):
+    return locals()
