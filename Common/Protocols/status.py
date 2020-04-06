5845ceb460950a99430dc785557fbb911fdc889f
Ashley Sands <ashsands1@amstrike.com>
2020-04-06 15:54:24 +0100

    added new client status

--- Common/Protocols/status.py
+++ Common/Protocols/status.py
@@ -15,7 +15,7 @@ def server_status(from_client_name, status_type, ok, message):
 # client status types
 CS_CLIENT           = 0
 CS_SCENE_LOADED     = 1
-
+CS_GAME_READY       = 2
 
 def client_status(from_client_name, status_type, ok, message):
     return locals()
