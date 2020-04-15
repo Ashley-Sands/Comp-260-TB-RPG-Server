8024c4d39ee9ff8cd980000f1b8a4ca392eb6fff
Ashley Sands <ashsands1@amstrike.com>
2020-04-16 00:35:30 +0100

    add wait till exit trigger to yeilld for seconds

--- Common/yield_for_seconds.py
+++ Common/yield_for_seconds.py
@@ -3,14 +3,14 @@ import time
 def yield_for_seconds(t, exit_func=None, intervals=0.000001):
     """
 
-    :param t:           amount of time to yield for
+    :param t:           amount of time to yield for. if t < 0 until the exit function is trigger
     :param exit_func:   function for erly exit. if returns true exit erly
     :yields:            True when compleatw
     """
 
     end_time = time.time() + t
 
-    while time.time() < end_time:
+    while time.time() < end_time or (t < 0 and exit_func is not None):
         yield False
         time.sleep(intervals)    # sleep for 1 nano
         if exit_func is not None and exit_func():
