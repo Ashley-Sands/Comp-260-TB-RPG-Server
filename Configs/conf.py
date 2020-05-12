bf1df0fcf2394323f18d1216688d8e5156f9d860
Ashley Sands <ashsands1@amstrike.com>
2020-05-12 23:28:05 +0100

    Added support for secrets, and updated .gitignore to prevent secrets getting synced with git :)

---
+++ Configs/conf.py
@@ -0,0 +1,30 @@
+import socket
+import Configs.secrets as secrets
+
+# config to run all servers as containers in a single docker instance
+# config files must contain the var conf as a dict.
+
+host_name = socket.gethostname()
+ip = socket.gethostbyname( host_name )
+
+secrets.Secrets.load()
+
+conf = {
+    # inbound connections, from the outside world :)
+    "host": "0.0.0.0",
+    "port": 8222,
+
+    # dynamic host addresses for local connections
+    "internal_host": ip,
+    "internal_port": 8223,
+
+    # fixed host, local connections
+    "internal_host_auth": "localhost_auth",
+    "internal_host_lobbies": "localhost_lobbies",
+
+    # mysql
+    "mysql_host": "localhost",
+    "mysql_user": "root",
+    "mysql_pass": secrets.Secrets.store["sql_password"]
+
+}
---
+++ Configs/secrets.py
@@ -0,0 +1,27 @@
+import os
+
+
+class Secrets:
+
+    loaded = False
+    store = {}
+
+    @staticmethod
+    def load(reload=False):
+
+        if Secrets.loaded and not reload:
+            return
+
+        # Secrets file are in CSV format. ie
+        # secret key one, secrete value one
+        # secret key two, secrete value two
+        files = os.listdir( "Configs/Secrets/" )
+
+        for f in files:
+            if f != "SHOWFILE":
+                with open(f) as file:
+                    secrets_lines = file.readlines()  # format key, value
+                    # add the values to the secrets store.
+                    for l in secrets_lines:
+                        key, value = l.split(",")
+                        Secrets.store[key] = value
